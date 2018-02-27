b45d3d47cd446fa04e075fbf491640d16ea4b5ae
Efim Poleschuk <limitlessFIMA@gmail.com>
2018-02-27 16:44:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let Stack = [];
+  
+  for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if(str.charAt(i) == bracketsConfig[j][0] && bracketsConfig[j][0] == bracketsConfig[j][1]) {
+        if(str.charAt(i) == Stack[Stack.length -1]) {
+          Stack.pop();
+        } else {
+          Stack.push(str.charAt(i));
+        }
+      } else {
+        if(str.charAt(i) == bracketsConfig[j][0]) {
+          Stack.push(str.charAt(i));
+        } else if(str.charAt(i) == bracketsConfig[j][1]) {
+          if(Stack.pop() != bracketsConfig[j][0]) {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  if(Stack.length != 0) {
+    return false;
+  }
+  return true;
 }
